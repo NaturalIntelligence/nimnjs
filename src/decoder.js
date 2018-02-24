1e613f6f2c573623c3d100702d49c856bc098bb4
Amit Gupta <amitgupta.gwl@gmail.com>
2018-02-24 17:29:37 +0530

    remove callbacks to improve performance on nodejs 9.5

--- src/decoder.js
+++ src/decoder.js
@@ -11,36 +11,32 @@ decoder.prototype._d = function(schema){
         var key = keys[i];
         var nextKey = keys[i+1];
         var schemaOfCurrentKey = properties[key];
-        if(this.currentChar() === chars.boundryChar/*  || fieldVal === "" */) {
+        if(this.dataToDecode[this.index] === chars.boundryChar) {
             this.index++; 
         } 
         if(schemaOfCurrentKey.type.value === dataType.ARRAY.value){
             var itemSchema = schemaOfCurrentKey.properties; //schema of array item
             var item = getKey(itemSchema,0);
-            if(this.currentChar() === chars.nilChar){
+            if(this.dataToDecode[this.index] === chars.nilChar){
                 this.index++;
-            }else if(this.currentChar() === chars.emptyChar){
+            }else if(this.dataToDecode[this.index] === chars.emptyChar){
                 obj[key] = [];
                 this.index++;
-            }else if(this.currentChar() !== chars.arrStart){
+            }else if(this.dataToDecode[this.index] !== chars.arrStart){
                 throw Error("Parsing error: Array start char was expcted");
             }else{
                 this.index++;
                 obj[key] = []
                 do{
                     if(item.properties){
-                        this.processObject(item, function(result){
-                            obj[key].push(result);
-                        });
+                        this.processObject(obj,key,item,true);
                     }else{
                         this.processPremitiveValue(obj,key,item,true);
                     }   
-                }while(this.currentChar() === chars.arraySepChar && ++this.index);
+                }while(this.dataToDecode[this.index] === chars.arraySepChar && ++this.index);
             }
         }else if(schemaOfCurrentKey.type.value === dataType.OBJECT.value){
-            this.processObject(schemaOfCurrentKey, function(result){
-                obj[key] = result;
-            });
+            this.processObject(obj,key,schemaOfCurrentKey);
         }else{
             this.processPremitiveValue(obj,key,schemaOfCurrentKey);
         }
@@ -49,19 +45,27 @@ decoder.prototype._d = function(schema){
 }
 
 
-decoder.prototype.processObject = function(item,callback){
-    if(this.currentChar() === chars.nilChar){
+decoder.prototype.processObject = function(obj,key,item,isArr){
+    if(this.dataToDecode[this.index] === chars.nilChar){
         this.index++;
-    }else if(this.currentChar() === chars.emptyChar){
-        callback({});
+    }else if(this.dataToDecode[this.index] === chars.emptyChar){
+        if(isArr){
+            obj[key].push({});
+        }else{
+            obj[key] = {};
+        }
         this.index++;
-    }else if(this.currentChar() !== chars.objStart){
+    }else if(this.dataToDecode[this.index] !== chars.objStart){
         throw Error("Parsing error: Object start char was expcted");
     }else{
         this.index++;
         var result = this._d(item);
         if(result !== undefined){
-            callback(result);
+            if(isArr){
+                obj[key].push(result);
+            }else{
+                obj[key] = result;
+            }
         }
     }
 }
@@ -74,7 +78,7 @@ decoder.prototype.currentChar = function(){
 }
 
 
-decoder.prototype.processPremitiveValue = function(obj,key,schemaOfCurrentKey,isArray){
+decoder.prototype.processPremitiveValue = function(obj,key,schemaOfCurrentKey,isArr){
     var val = "";
     if(schemaOfCurrentKey.readUntil){
         val = this.readFieldValue(schemaOfCurrentKey.readUntil);
@@ -88,8 +92,8 @@ decoder.prototype.processPremitiveValue = function(obj,key,schemaOfCurrentKey,is
     if(this.dataToDecode[this.index] === chars.boundryChar) this.index++;
     if(val !== chars.nilPremitive){
         schemaOfCurrentKey.type.parseBack(val,function(result){
-            if(isArray){
-                (obj[key] = obj[key] || []).push(result);
+            if(isArr){
+                obj[key].push(result);
             }else{
                 obj[key] = result;
             }
