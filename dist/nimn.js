71ed70c4c72636cbb3f861b911a41ae38e76e436
Amit Gupta <amitgupta.gwl@gmail.com>
2018-04-28 11:14:50 +0530

    fix boolean char

--- dist/nimn.js
+++ dist/nimn.js
@@ -429,8 +429,8 @@ module.exports = nimn;
 var chars = require("../chars").chars;
 var char = require("../util").char;
 
-var yes = char(217);
-var no = char(218);
+var yes = char(181);
+var no = char(183);
 
 booleanCharset = {};
 booleanCharset[yes] = true;
--- src/parsers/boolean.js
+++ src/parsers/boolean.js
@@ -1,8 +1,8 @@
 var chars = require("../chars").chars;
 var char = require("../util").char;
 
-var yes = char(217);
-var no = char(218);
+var yes = char(181);
+var no = char(183);
 
 booleanCharset = {};
 booleanCharset[yes] = true;
